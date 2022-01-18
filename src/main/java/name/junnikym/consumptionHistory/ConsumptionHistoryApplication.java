bb18e3cc716a949e484c6bc8bfa6fd885ea445cf
junnikym <junnikym@gmail.com>
2022-01-18 15:58:23 +0900

    Init SpringBoot Project

---
+++ src/main/java/name/junnikym/consumptionHistory/ConsumptionHistoryApplication.java
@@ -0,0 +1,13 @@
+package name.junnikym.consumptionHistory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsumptionHistoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsumptionHistoryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/name/junnikym/consumptionHistory/ConsumptionHistoryApplicationTests.java
@@ -0,0 +1,13 @@
+package name.junnikym.consumptionHistory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsumptionHistoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
